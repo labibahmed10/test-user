8bc946423a00ed446bae06cc87ab555967aed2bf
labibahmed10 <labib.ahmed.372@gmail.com>
2023-06-22 23:38:53 +0600

    responsive made only for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 function App() {
    return (
-      <div className="background h-screen">
-         <section className="md:w-[60rem] md:h-[50rem] mx-auto h-screen py-32 ">
-            <h1 className="text-4xl font-bold text-center text-[#003049]">
+      <div className="background md:h-screen">
+         <section className="md:w-[60rem] md:h-[50rem] mx-auto h-screen mmd:py-32 py-40">
+            <h1 className="md:text-4xl text-2xl font-bold text-center text-[#003049]">
                Please enter your name and pick the Sectors you are currently involved in
             </h1>
 
-            <form action="" className="w-full h-full">
-               <div className="pl-12">
-                  <label htmlFor="name" className="text-2xl font-semibold">
+            <form action="" className="w-full h-full md:mt-20 mt-14">
+               <div className="md:pl-12 pl-4">
+                  <label htmlFor="name" className="md:text-2xl text-lg font-semibold">
                      Name :
                   </label>
                   <input
                      type="text"
                      name="name"
                      id="name"
-                     className="ml-2 border border-slate-300 bg-gray-50 rounded-md focus:outline-0 px-3 py-3 w-[45%] mt-20 text-xl"
+                     className="md:ml-2 ml-1 border border-slate-300 bg-gray-50 rounded-md focus:outline-0 md:px-3 px-2 md:py-3 py-2 md:w-[45%] text-xl"
                   />
                </div>
 
-               <div className="pl-12 mt-5">
-                  <label htmlFor="sectors" className="text-2xl font-semibold">
+               <div className="md:pl-12 pl-4 md:mt-5 mt-4">
+                  <label htmlFor="sectors" className="md:text-2xl text-lg font-semibold">
                      Sectors :
                   </label>
 
                   <select
                      name="sectors"
                      id="sectors"
-                     className="px-4 py-4 w-[43.5%] border border-slate-300 focus:outline-0 rounded-md bg-gray-50 ml-2 text-xl"
+                     className="md:px-4 px-2 md:py-4 py-3 md:w-[43.5%] w-[73%] border border-slate-300 focus:outline-0 rounded-md bg-gray-50 md:ml-2 ml-1 md:text-xl"
                   >
                      <optgroup label="many">Many</optgroup>
                      <option value="">Optisonsa</option>
@@ -36,14 +36,14 @@ function App() {
                   </select>
                </div>
 
-               <div className="flex items-center pl-12 mt-5">
+               <div className="flex items-center md:pl-12 pl-4 md:mt-5 mt-5">
                   <input
                      id="checkbox"
                      type="checkbox"
                      value=""
-                     className="w-6 h-6 rounded-full mr-3"
+                     className="md:w-6 w-4 md:h-6 h-4 rounded-full md:mr-3 mr-2"
                   ></input>
-                  <label htmlFor="checkbox" className="text-2xl font-semibold">
+                  <label htmlFor="checkbox" className="md:text-2xl text-lg font-semibold">
                      Agree to terms
                   </label>
                </div>
@@ -51,7 +51,7 @@ function App() {
                <input
                   type="submit"
                   value="Save"
-                  className="bg-[#003049] text-slate-50 text-2xl px-6 py-2 rounded-full mt-8 ml-12"
+                  className="bg-[#003049] text-slate-50 md:text-2xl text-xl md:px-6 px-5 md:py-2 py-1 rounded-full md:mt-8 mt-5 md:ml-12 ml-4"
                />
             </form>
          </section>
